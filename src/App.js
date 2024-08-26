48f72bfb8fee3b9daaf5c8f1db33ad84275a47e8
abdulraheem88 <168866788+abdulraheem88@users.noreply.github.com>
2024-08-27 01:34:19 +0500

    refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import Shop from './components/Shop'
+import Shop from './components/Shop';
+// Import other components as needed
+
 function App() {
   return (
-    <div className="App">
-     <Navbar/>
-     <Shop/>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          {/* Add more routes here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
